0964d7c02919009b86e24d8e2064b6e891681da2
vini.wessner@gmail.com <vini.wessner@gmail.com>
2024-11-03 17:03:52 -0300

    created validations fields

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/ViniciusWessner/Go-Crud/src/configuration/rest_err"
+	"github.com/ViniciusWessner/Go-Crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context){
-	err := rest_err.NewBadRequestError("Voce chamou a rota errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindBodyWithJSON(&userRequest); err != nil{
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("Temos alguns campos incorretos, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
