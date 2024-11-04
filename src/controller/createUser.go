9473ca6620e19fba7ca17a11adf4a2cf46c7898f
vini.wessner@gmail.com <vini.wessner@gmail.com>
2024-11-04 18:44:47 -0300

    add logger this project

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,22 +1,36 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/ViniciusWessner/Go-Crud/src/configuration/logger"
 	"github.com/ViniciusWessner/Go-Crud/src/configuration/validation"
 	"github.com/ViniciusWessner/Go-Crud/src/controller/model/request"
+	"github.com/ViniciusWessner/Go-Crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
-func CreateUser(c *gin.Context){
+func CreateUser(c *gin.Context) {
+	logger.Info("Iniciando CreateUser controller",
+		zap.String("Jornada", "CreateUser"))
 	var userRequest request.UserRequest
 
-	if err := c.ShouldBindJSON(&userRequest); err != nil{
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Erro ao validar informacoes do usuario", err,
+			zap.String("Jornada", "CreateUser"))
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
 
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
